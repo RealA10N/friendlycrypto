d1afb232403b7e5561b6e3aac3fbe0fbbe77bf23
Alon (PC) <downtown2u@gmail.com>
2021-07-16 13:22:37 +0300

    ✅ Added more tests test for basic `Cryptographer`

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -13,11 +13,14 @@ from friendlycrypto import Cryptographer
     os.urandom(1024),
 ))
 @pytest.mark.parametrize('data', (
+    bytes(0),
     b'secured data',
 ))
 @pytest.mark.parametrize('password', (
     b'MySecur3Pas5w0rd',
     base64.b64encode(b'This is my password!\n'),
+    'MyPassword'.encode('utf8'),
+    int.to_bytes(2102, 2, 'big'),
 ))
 def test_encryption_decryption(salt, data, password):
     crypto = Cryptographer(salt=salt, kdf_iterations=1)
